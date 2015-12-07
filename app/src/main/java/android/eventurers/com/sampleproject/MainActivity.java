2d02c494c4e07677475e49f9d490e994dc6de9dc
sackurise <ram.eventurers@gmail.com>
2015-12-07 16:18:20 +0530

    Second Commit

--- app/src/main/java/android/eventurers/com/sampleproject/MainActivity.java
+++ app/src/main/java/android/eventurers/com/sampleproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setSubtitle("Cheese of Main Activity");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

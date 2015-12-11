a9fdfb85b62a56d5fe4f80c3572456b0dc6ee894
sackurise <ram.eventurers@gmail.com>
2015-12-11 12:55:36 +0530

    Third upload

--- app/src/main/java/android/eventurers/com/sampleproject/SecondActivity.java
+++ app/src/main/java/android/eventurers/com/sampleproject/SecondActivity.java
@@ -1,24 +1,36 @@
 package android.eventurers.com.sampleproject;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class SecondActivity extends AppCompatActivity {
     private FloatingActionButton fab;
-    private ImageView imageView;
+    private ImageView imageView, imgAnimated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         init();
+        setDefaults();
         setupEvents();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,6 +43,7 @@ public class SecondActivity extends AppCompatActivity {
     private void init()
     {
         imageView = (ImageView)findViewById(R.id.backdrop);
+        imgAnimated = (ImageView)findViewById(R.id.animimage);
         fab = (FloatingActionButton) findViewById(R.id.fab);
     }
 
@@ -48,10 +61,16 @@ public class SecondActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Intent intent = new Intent(SecondActivity.this, HomeSlidingViewPager.class);
+                startActivity(intent);
             }
         });
     }
 
+    private void setDefaults()
+    {
+        Animation animation = AnimationUtils.loadAnimation(SecondActivity.this, R.anim.animforrepeatimage);
+        imgAnimated.setAnimation(animation);
+    }
 }
